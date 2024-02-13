9100cec242d0a590246080855e96f6dde0d9825e
hp-77 <harsh.pant.77@gmail.com>
2024-02-13 13:13:00 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -18,8 +18,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
-app.use("/api/chats", chatRoute);
-app.use("/api/messages", messageRoute);
+//app.use("/api/chats", chatRoute);
+//app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");
